85da8102a9e5e754f8649c7de8ece3f12523f9bb
deanwade9 <546833822@qq.com>
2021-02-09 01:46:46 +0800

    项目构建 路由  elementUI集成

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    <el-button>默认按钮11111</el-button>
+  </div>
+</template>
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="scss">
+#login {
+  height: 100vh;
+  background-color: rgb(240, 240, 241);
+}
+</style>
